e85efc156e6905bdad6626bc87519f9c4dde68a5
S Fujimoto <unicoshun@gmail.com>
2017-08-22 22:17:47 +0900

    ~156

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,37 +1,29 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title'},
+  { label: 'Subject Title', name: 'subject'},
+  { label: 'Email Body', name: 'body'},
+  { label: 'Recipient List', name: 'emails'}
+];
 
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
+    return _.map(FIELDS, ({ label, name}) => {
+      return (
         <Field
-          label="Survey Title"
-          type="text"
-          name="title"
+          key={name}
           component={SurveyField}
-        />
-        <Field
-          label="Subject Title"
           type="text"
-          name="subject"
-          component={SurveyField}
-        />
-        <Field
-          label="Email Body"
-          type="text"
-          name="body"
-          component={SurveyField}
-        />
-        <Field
-          label="Recipient List"
-          type="text"
-          name="emails"
-          component={SurveyField}
-        />
-      </div>
-    );
+          label={label}
+          name={name}/>
+      );
+    });
   }
 
   render() {
@@ -39,13 +31,37 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     )
   }
 }
+// values comes from our form eg. {body: , emails: , subject: ...
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = 'You must provide a value';
+    }
+  });
+
+
+
+  return errors;
+}
+
 // handleSubmit is a func from redux-form
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm);
